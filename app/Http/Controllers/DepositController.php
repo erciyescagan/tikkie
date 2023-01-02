7ee81f822802417d6ddc99938319066b297eafe1
erciyescagan <erciyescagan@gmail.com>
2023-01-02 23:26:12 +0100

    Requests has been created according to #19

--- app/Http/Controllers/DepositController.php
+++ app/Http/Controllers/DepositController.php
@@ -3,27 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Http\Helpers\Json;
+use App\Http\Requests\DepositRequest;
 use App\Models\Deposit;
 use Illuminate\Http\Request;
 
 class DepositController extends Controller
 {
-    public function __construct(Request $request){
-        if ($request->route()->getPrefix() == "api") {
-            $request->validate([
-                "amount" => "numeric|required",
-            ]);
 
-            Json::setJson($request);
-
-        }
-
-    }
-
-    public function store(Request $request){
+    public function store(DepositRequest $request){
         $deposit = new Deposit();
         $deposit->user_id = $request->user()->id;
-        $deposit->amount = Json::getJson()['amount'];
+        $deposit->amount = $request->amount;
         $deposit->status = 'success';
         if ($deposit->save()){
             return response()->json(['status' => 'success', 'statusCode' => 200,'message' => 'Para yatırma işleminiz başarıyla tamamlandı!', 'data' => ['deposit' => $deposit], 200]);
